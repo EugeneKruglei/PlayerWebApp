ed026f84384cbc24338dfde1dc58ba46126b5e7e
EugeneKruglei <eugene787@yandex.ru>
2018-11-22 11:01:54 +0300

    initial-intern-project

--- src/App.js
+++ src/App.js
@@ -1,28 +1,54 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Page from './page';
+import Nav from './nav';
+import Link from './link';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+    render() {
+        return (
+            <div className="App">
+                <div>
+                    <Link to="/main" >
+                        <button>Main</button>
+                    </Link>
+                    <Link to="/product" >
+                        <button>Product</button>
+                    </Link>
+                    <Link to="/about" >
+                        <button>About</button>
+                    </Link>
+                    <Link to="/contact" >
+                        <button>Contact</button>
+                    </Link>
+                </div>
+                <Nav>
+                    <Page path="/main">
+                        <div className="page main">
+                            main
+                        </div>
+                    </Page>
+                    <Page path="/product">
+                        <div className="page product">
+                            Product
+                        </div>
+                    </Page>
+                    <Page path="/about">
+                        <div className="page about">
+                            About
+                        </div>
+                    </Page>
+                    <Page path="/contact">
+                        <div className="page contact">
+                            Contact
+                        </div>
+                    </Page>
+                </Nav>
+
+            </div>
+        );
+    }
 }
 
 export default App;
